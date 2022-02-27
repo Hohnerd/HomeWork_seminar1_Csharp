7e7a76155a60bd648aadbb7ef990567f2c77186b
Hohnerd <kaloshin1977@yandex.ru>
2022-02-27 16:12:49 +0500

    Add zadacha_2

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+
+Console.WriteLine("Введите первое число А");
+int a=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите первое число B");
+int b=Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+{
+    Console.WriteLine("Число А является большим чем B");
+} 
+else
+{
+    Console.WriteLine("Число B является большим чем A");
+}
